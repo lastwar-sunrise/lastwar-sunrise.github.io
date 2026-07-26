eeeb3d6b10ebac4088e975636d42e34538a7085f
sunriser <273940016+cvunk-commits@users.noreply.github.com>
2026-07-26 21:03:55 +0800

    Update config.js

--- train/config.js
+++ train/config.js
@@ -1,2 +1,2 @@
-const SupabaseUrl = "https://vvgsljhqlefxzmvengao.supabase.co/rest/v1/";
+const SupabaseUrl = "https://vvgsljhqlefxzmvengao.supabase.co/";
 const SupabasePublishableKey = "sb_publishable_r0A9GRvO-vLgm3Imwi-JgQ_CZtf-tOY";
